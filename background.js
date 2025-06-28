d90f3ef855dbcede2f00c5d02b31901e40b6fc58
Takeshi Kunimi <takeshi.kunimi@umitron.com>
2025-06-28 17:09:35 +0900

    initial commit

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.action.onClicked.addListener((tab) => {
+  const currentUrl = tab.url;
+  const prompt = encodeURIComponent(`${currentUrl} を日本語で要約してください`);
+  const chatGptUrl = `https://chat.openai.com/?model=o4-mini&q=${prompt}`;
+  
+  chrome.tabs.create({ url: chatGptUrl });
+});
