79c5785e50855d4f1790a1eed7e4fa9aba5be0f2
Takeshi Kunimi <takeshi.kunimi@umitron.com>
2025-11-09 02:00:22 +0900

    Remove model param
    Now o4-mini is not available and the default model GPT-5 is OK.

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.action.onClicked.addListener((tab) => {
   const currentUrl = tab.url;
   const prompt = encodeURIComponent(`${currentUrl} を日本語で要約してください`);
-  const chatGptUrl = `https://chat.openai.com/?model=o4-mini&q=${prompt}`;
+  const chatGptUrl = `https://chat.openai.com/?q=${prompt}`;
   
   chrome.tabs.create({ url: chatGptUrl });
 });
